142f603f6d07122e1e04558161810b6788783799
MitchellTaylor <42538495+MitchellTaylor@users.noreply.github.com>
2018-10-10 15:13:01 +0100

    Add files via upload

--- maths.js
+++ maths.js
@@ -1,36 +1,206 @@
-/* --------------------------
-All calculations for bet types
+Single = new Array()
+Single[1] = "1"
 
-=+=+=+=
-KEY
-Keyword = codeword
-=+=+=+=
+Double = new Array()
+Double[1] = "1,2"
 
-Stake = userstake
-First odd = oddOne (Second first odd = oddOne2 etc)
-Second odd = oddTwo (Second second odd = oddTwo2 etc)
-Returns = betReturns
-Double returns = betDoubleReturns
+Treble = new Array()
+Treble[1] = "1,2,3"
 
--------------------------- */
+Accumulator4 = new Array()
+Accumulator4[1] = "1,2,3,4"
 
-// var singleEvent = document.getElementById("calculate1").addEventListener('click',buttonClick)
+Accumulator5 = new Array()
+Accumulator5[1] = "1,2,3,4,5"
 
-// function buttonClick(){
+Accumulator6 = new Array()
+Accumulator6[1] = "1,2,3,4,5,6"
 
-    
-//         var numerator1 = document.getElementById("numerator1").value;
-//         var stake = document.getElementById("stake").value;
-//         var denominator1 = document.getElementById("denominator1").value;
-//         var single = ((parseInt(numerator1) / parseInt(denominator1)) * stake) + stake;
-    
-//         document.getElementById("returnsResult").innerHTML = single;
-    
-//     }
+Accumulator7 = new Array()
+Accumulator7[1] = "1,2,3,4,5,6,7"
 
-// var stake = 4;
-// var oddOne = 4;
-// var oddTwo = 1;
+Accumulator8 = new Array()
+Accumulator8[1] = "1,2,3,4,5,6,7,8"
+
+Accumulator9 = new Array()
+Accumulator9[1] = "1,2,3,4,5,6,7,8,9"
+
+Accumulator10 = new Array()
+Accumulator10[1] = "1,2,3,4,5,6,7,8,9,10"
+
+Trixie = new Array()
+Trixie[1] = "1,2,3"
+Trixie[2] = "1,2"
+Trixie[3] = "1,3"
+Trixie[4] = "2,3"
+
+Patent = new Array()
+Patent[1] = "1,2,3"
+Patent[2] = "1,2"
+Patent[3] = "1,3"
+Patent[4] = "2,3"
+Patent[5] = "1"
+Patent[6] = "2"
+Patent[7] = "3"
+
+Yankee = new Array()
+Yankee[1] = "1,2,3,4"
+Yankee[2] = "1,2,3"
+Yankee[3] = "1,2,4"
+Yankee[4] = "2,3,4"
+Yankee[5] = "1,3,4"
+Yankee[6] = "1,2"
+Yankee[7] = "1,3"
+Yankee[8] = "1,4"
+Yankee[9] = "2,3"
+Yankee[10] = "2,4"
+Yankee[11] = "3,4"
+
+Lucky15 = new Array()
+Lucky15[1] = "1,2,3,4"
+Lucky15[2] = "1,2,3"
+Lucky15[3] = "1,2,4"
+Lucky15[4] = "2,3,4"
+Lucky15[5] = "1,3,4"
+Lucky15[6] = "1,2"
+Lucky15[7] = "1,3"
+Lucky15[8] = "1,4"
+Lucky15[9] = "2,3"
+Lucky15[10] = "2,4"
+Lucky15[11] = "3,4"
+Lucky15[12] = "1"
+Lucky15[13] = "2"
+Lucky15[14] = "3"
+Lucky15[15] = "4"
+
+Canadian = new Array()
+Canadian[1] = "1,2,3,4,5"
+Canadian[2] = "1,2,3,4"
+Canadian[3] = "1,2,3,5"
+Canadian[4] = "1,2,4,5"
+Canadian[5] = "1,3,4,5"
+Canadian[6] = "2,3,4,5"
+Canadian[7] = "2,4,5"
+Canadian[8] = "1,2,3"
+Canadian[9] = "1,2,4"
+Canadian[10] = "1,2,5"
+Canadian[11] = "1,3,4"
+Canadian[12] = "1,3,5"
+Canadian[13] = "1,4,5"
+Canadian[14] = "2,3,4"
+Canadian[15] = "2,3,5"
+Canadian[16] = "3,4,5"
+Canadian[17] = "1,2"
+Canadian[18] = "1,3"
+Canadian[19] = "1,4"
+Canadian[20] = "1,5"
+Canadian[21] = "2,3"
+Canadian[22] = "2,4"
+Canadian[23] = "2,5"
+Canadian[24] = "3,4"
+Canadian[25] = "3,5"
+Canadian[26] = "4,5"
+
+Lucky31 = new Array()
+Lucky31[1] = "1,2,3,4,5"
+Lucky31[2] = "1,2,3,4"
+Lucky31[3] = "1,2,3,5"
+Lucky31[4] = "1,2,4,5"
+Lucky31[5] = "1,3,4,5"
+Lucky31[6] = "2,3,4,5"
+Lucky31[7] = "2,4,5"
+Lucky31[8] = "1,2,3"
+Lucky31[9] = "1,2,4"
+Lucky31[10] = "1,2,5"
+Lucky31[11] = "1,3,4"
+Lucky31[12] = "1,3,5"
+Lucky31[13] = "1,4,5"
+Lucky31[14] = "2,3,4"
+Lucky31[15] = "2,3,5"
+Lucky31[16] = "3,4,5"
+Lucky31[17] = "1,2"
+Lucky31[18] = "1,3"
+Lucky31[19] = "1,4"
+Lucky31[20] = "1,5"
+Lucky31[21] = "2,3"
+Lucky31[22] = "2,4"
+Lucky31[23] = "2,5"
+Lucky31[24] = "3,4"
+Lucky31[25] = "3,5"
+Lucky31[26] = "4,5"
+Lucky31[27] = "1"
+Lucky31[28] = "2"
+Lucky31[29] = "3"
+Lucky31[30] = "4"
+Lucky31[31] = "5"
+
+Lucky63 = new Array()
+Lucky63[1] = "1,2,3,4,5,6"
+Lucky63[2] = "1,2,3,4,5"
+Lucky63[3] = "1,2,3,4,6"
+Lucky63[4] = "1,2,3,5,6"
+Lucky63[5] = "1,2,4,5,6"
+Lucky63[6] = "1,3,4,5,6"
+Lucky63[7] = "2,3,4,5,6"
+Lucky63[8] = "1,2,3,4"
+Lucky63[9] = "1,2,3,5"
+Lucky63[10] = "1,2,3,6"
+Lucky63[11] = "1,2,4,5"
+Lucky63[12] = "1,2,4,6"
+Lucky63[13] = "1,2,5,6"
+Lucky63[14] = "1,3,4,5"
+Lucky63[15] = "1,3,4,6"
+Lucky63[16] = "1,4,5,6"
+Lucky63[17] = "2,3,4,5"
+Lucky63[18] = "2,3,5,6"
+Lucky63[19] = "2,4,5,6"
+Lucky63[20] = "3,4,5,6"
+Lucky63[21] = "2,3,4,6"
+Lucky63[22] = "1,3,4,6"
+Lucky63[23] = "1,2,3"
+Lucky63[24] = "1,2,4"
+Lucky63[25] = "1,2,5"
+Lucky63[26] = "1,2,6"
+Lucky63[27] = "1,3,4"
+Lucky63[28] = "1,3,5"
+Lucky63[29] = "1,3,6"
+Lucky63[30] = "1,4,5"
+Lucky63[31] = "1,4,6"
+Lucky63[32] = "1,5,6"
+Lucky63[33] = "2,3,4"
+Lucky63[34] = "2,3,5"
+Lucky63[35] = "2,3,6"
+Lucky63[36] = "2,4,5"
+Lucky63[37] = "2,4,6"
+Lucky63[38] = "2,5,6"
+Lucky63[39] = "3,4,5"
+Lucky63[40] = "3,4,6"
+Lucky63[41] = "3,5,6"
+Lucky63[42] = "4,5,6"
+Lucky63[43] = "1,2"
+Lucky63[44] = "1,3"
+Lucky63[45] = "1,4"
+Lucky63[46] = "1,5"
+Lucky63[47] = "1,6"
+Lucky63[48] = "2,3"
+Lucky63[49] = "2,4"
+Lucky63[50] = "2,5"
+Lucky63[51] = "2,6"
+Lucky63[52] = "3,4"
+Lucky63[53] = "3,5"
+Lucky63[54] = "3,6"
+Lucky63[55] = "4,5"
+Lucky63[56] = "4,6"
+Lucky63[57] = "5,6"
+Lucky63[58] = "1"
+Lucky63[59] = "2"
+Lucky63[60] = "3"
+Lucky63[61] = "4"
+Lucky63[62] = "5"
+Lucky63[63] = "6"
+
+
+
 
 // var betReturns = ((oddOne / oddTwo) * stake) + stake;
 // var betDoubleReturns = ((oddOne / oddTwo) * betReturns) + betReturns;
@@ -67,53 +237,3 @@ Double returns = betDoubleReturns
 // console.log(canadianReturns + " - Canadian returns");   //31000
 
 
-/* --------------------------
-
-Single
-
-((oddOne / oddTwo) * stake) + stake = betReturns (20)
-
--------------------------- */
-
-/* --------------------------
-
-Double
-
-((oddOne / oddTwo) * stake) + stake = betReturns (20)
-((oddOne2 / oddTwo2) * betReturns) + betReturns = betDoubleReturns (100)
-
--------------------------- */
-
-/* --------------------------
-
-Treble
-
-((oddOne / oddTwo) * stake) + stake = betReturns (20)
-((oddOne2 / oddTwo2) * betReturns) + betReturns = betDoubleReturns (100)
-((oddOne3 / oddTwo3) * betDoubleReturns) + betDoubleReturns = betTrebleReturns (500)
-
--------------------------- */
-
-/* --------------------------
-
-Fourfold
-
-((oddOne / oddTwo) * stake) + stake = betReturns (20)
-((oddOne2 / oddTwo2) * betReturns) + betReturns = betDoubleReturns (100)
-((oddOne3 / oddTwo3) * betDoubleReturns) + betDoubleReturns = betTrebleReturns (500)
-((oddOne4 / oddTwo4) * betTrebleReturns) + betTrebleReturns = bet4FoldReturns (2500)
-
--------------------------- */
-
-/* --------------------------
-
-Lucky 15 (4 Singles, 6 Doubles, 4 Trebles & 1 Fourfold)
-
-((oddOne / oddTwo) * stake) + stake = betReturns (20)
-((oddOne2 / oddTwo2) * betReturns) + betReturns = betDoubleReturns (100)
-((oddOne3 / oddTwo3) * betDoubleReturns) + betDoubleReturns = betTrebleReturns (500)
-((oddOne4 / oddTwo4) * betTrebleReturns) + betTrebleReturns = bet4FoldReturns (2500)
-
-bet4FoldReturns + betTrebleReturns + betDoubleReturns + betReturns = bet15Returns (5180)
-
--------------------------- */
