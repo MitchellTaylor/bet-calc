c7790da9772709da1d1aa7c1b29b419d21ff6d2d
MitchellTaylor <42538495+MitchellTaylor@users.noreply.github.com>
2018-10-11 14:38:34 +0100

    Add files via upload

--- maths.js
+++ maths.js
@@ -1,204 +1,369 @@
-Single = new Array()
-Single[1] = "1"
+// ---- Arrays for bet combos
 
-Double = new Array()
-Double[1] = "1,2"
+var Single = new Array[]
+Single[1] = "1";
 
-Treble = new Array()
-Treble[1] = "1,2,3"
+var Double = new Array[]
+Double[1] = "1,2";
 
-Accumulator4 = new Array()
-Accumulator4[1] = "1,2,3,4"
+var Treble = new Array[]
+Treble[1] = "1,2,3";
 
-Accumulator5 = new Array()
-Accumulator5[1] = "1,2,3,4,5"
+var Accumulator4 = new Array[]
+Accumulator4[1] = "1,2,3,4";
 
-Accumulator6 = new Array()
-Accumulator6[1] = "1,2,3,4,5,6"
+var Accumulator5 = new Array[]
+Accumulator5[1] = "1,2,3,4,5";
 
-Accumulator7 = new Array()
-Accumulator7[1] = "1,2,3,4,5,6,7"
+var Accumulator6 = new Array[]
+Accumulator6[1] = "1,2,3,4,5,6";
 
-Accumulator8 = new Array()
-Accumulator8[1] = "1,2,3,4,5,6,7,8"
+var Accumulator7 = new Array[]
+Accumulator7[1] = "1,2,3,4,5,6,7";
 
-Accumulator9 = new Array()
-Accumulator9[1] = "1,2,3,4,5,6,7,8,9"
+var Accumulator8 = new Array[]
+Accumulator8[1] = "1,2,3,4,5,6,7,8";
 
-Accumulator10 = new Array()
-Accumulator10[1] = "1,2,3,4,5,6,7,8,9,10"
+var Accumulator9 = new Array[]
+Accumulator9[1] = "1,2,3,4,5,6,7,8,9";
 
-Trixie = new Array()
-Trixie[1] = "1,2,3"
-Trixie[2] = "1,2"
-Trixie[3] = "1,3"
-Trixie[4] = "2,3"
+var Accumulator10 = new Array[]
+Accumulator10[1] = "1,2,3,4,5,6,7,8,9,10";
 
-Patent = new Array()
-Patent[1] = "1,2,3"
-Patent[2] = "1,2"
-Patent[3] = "1,3"
-Patent[4] = "2,3"
-Patent[5] = "1"
-Patent[6] = "2"
-Patent[7] = "3"
+var Trixie = new Array[]
+Trixie[1] = "1,2,3";
+Trixie[2] = "1,2";
+Trixie[3] = "1,3";
+Trixie[4] = "2,3";
 
-Yankee = new Array()
-Yankee[1] = "1,2,3,4"
-Yankee[2] = "1,2,3"
-Yankee[3] = "1,2,4"
-Yankee[4] = "2,3,4"
-Yankee[5] = "1,3,4"
-Yankee[6] = "1,2"
-Yankee[7] = "1,3"
-Yankee[8] = "1,4"
-Yankee[9] = "2,3"
-Yankee[10] = "2,4"
-Yankee[11] = "3,4"
+var Patent = new Array[]
+Patent[1] = "1,2,3";
+Patent[2] = "1,2";
+Patent[3] = "1,3";
+Patent[4] = "2,3";
+Patent[5] = "1";
+Patent[6] = "2";
+Patent[7] = "3";
 
-Lucky15 = new Array()
-Lucky15[1] = "1,2,3,4"
-Lucky15[2] = "1,2,3"
-Lucky15[3] = "1,2,4"
-Lucky15[4] = "2,3,4"
-Lucky15[5] = "1,3,4"
-Lucky15[6] = "1,2"
-Lucky15[7] = "1,3"
-Lucky15[8] = "1,4"
-Lucky15[9] = "2,3"
-Lucky15[10] = "2,4"
-Lucky15[11] = "3,4"
-Lucky15[12] = "1"
-Lucky15[13] = "2"
-Lucky15[14] = "3"
-Lucky15[15] = "4"
+var Yankee = new Array[]
+Yankee[1] = "1,2,3,4";
+Yankee[2] = "1,2,3";
+Yankee[3] = "1,2,4";
+Yankee[4] = "2,3,4";
+Yankee[5] = "1,3,4";
+Yankee[6] = "1,2";
+Yankee[7] = "1,3";
+Yankee[8] = "1,4";
+Yankee[9] = "2,3";
+Yankee[10] = "2,4";
+Yankee[11] = "3,4";
 
-Canadian = new Array()
-Canadian[1] = "1,2,3,4,5"
-Canadian[2] = "1,2,3,4"
-Canadian[3] = "1,2,3,5"
-Canadian[4] = "1,2,4,5"
-Canadian[5] = "1,3,4,5"
-Canadian[6] = "2,3,4,5"
-Canadian[7] = "2,4,5"
-Canadian[8] = "1,2,3"
-Canadian[9] = "1,2,4"
-Canadian[10] = "1,2,5"
-Canadian[11] = "1,3,4"
-Canadian[12] = "1,3,5"
-Canadian[13] = "1,4,5"
-Canadian[14] = "2,3,4"
-Canadian[15] = "2,3,5"
-Canadian[16] = "3,4,5"
-Canadian[17] = "1,2"
-Canadian[18] = "1,3"
-Canadian[19] = "1,4"
-Canadian[20] = "1,5"
-Canadian[21] = "2,3"
-Canadian[22] = "2,4"
-Canadian[23] = "2,5"
-Canadian[24] = "3,4"
-Canadian[25] = "3,5"
-Canadian[26] = "4,5"
+var Lucky15 = new Array[]
+Lucky15[1] = "1,2,3,4";
+Lucky15[2] = "1,2,3";
+Lucky15[3] = "1,2,4";
+Lucky15[4] = "2,3,4";
+Lucky15[5] = "1,3,4";
+Lucky15[6] = "1,2";
+Lucky15[7] = "1,3";
+Lucky15[8] = "1,4";
+Lucky15[9] = "2,3";
+Lucky15[10] = "2,4";
+Lucky15[11] = "3,4";
+Lucky15[12] = "1";
+Lucky15[13] = "2";
+Lucky15[14] = "3";
+Lucky15[15] = "4";
 
-Lucky31 = new Array()
-Lucky31[1] = "1,2,3,4,5"
-Lucky31[2] = "1,2,3,4"
-Lucky31[3] = "1,2,3,5"
-Lucky31[4] = "1,2,4,5"
-Lucky31[5] = "1,3,4,5"
-Lucky31[6] = "2,3,4,5"
-Lucky31[7] = "2,4,5"
-Lucky31[8] = "1,2,3"
-Lucky31[9] = "1,2,4"
-Lucky31[10] = "1,2,5"
-Lucky31[11] = "1,3,4"
-Lucky31[12] = "1,3,5"
-Lucky31[13] = "1,4,5"
-Lucky31[14] = "2,3,4"
-Lucky31[15] = "2,3,5"
-Lucky31[16] = "3,4,5"
-Lucky31[17] = "1,2"
-Lucky31[18] = "1,3"
-Lucky31[19] = "1,4"
-Lucky31[20] = "1,5"
-Lucky31[21] = "2,3"
-Lucky31[22] = "2,4"
-Lucky31[23] = "2,5"
-Lucky31[24] = "3,4"
-Lucky31[25] = "3,5"
-Lucky31[26] = "4,5"
-Lucky31[27] = "1"
-Lucky31[28] = "2"
-Lucky31[29] = "3"
-Lucky31[30] = "4"
-Lucky31[31] = "5"
+var Canadian = new Array[]
+Canadian[1] = "1,2,3,4,5";
+Canadian[2] = "1,2,3,4";
+Canadian[3] = "1,2,3,5";
+Canadian[4] = "1,2,4,5";
+Canadian[5] = "1,3,4,5";
+Canadian[6] = "2,3,4,5";
+Canadian[7] = "2,4,5";
+Canadian[8] = "1,2,3";
+Canadian[9] = "1,2,4";
+Canadian[10] = "1,2,5";
+Canadian[11] = "1,3,4";
+Canadian[12] = "1,3,5";
+Canadian[13] = "1,4,5";
+Canadian[14] = "2,3,4";
+Canadian[15] = "2,3,5";
+Canadian[16] = "3,4,5";
+Canadian[17] = "1,2";
+Canadian[18] = "1,3";
+Canadian[19] = "1,4";
+Canadian[20] = "1,5";
+Canadian[21] = "2,3";
+Canadian[22] = "2,4";
+Canadian[23] = "2,5";
+Canadian[24] = "3,4";
+Canadian[25] = "3,5";
+Canadian[26] = "4,5";
 
-Lucky63 = new Array()
-Lucky63[1] = "1,2,3,4,5,6"
-Lucky63[2] = "1,2,3,4,5"
-Lucky63[3] = "1,2,3,4,6"
-Lucky63[4] = "1,2,3,5,6"
-Lucky63[5] = "1,2,4,5,6"
-Lucky63[6] = "1,3,4,5,6"
-Lucky63[7] = "2,3,4,5,6"
-Lucky63[8] = "1,2,3,4"
-Lucky63[9] = "1,2,3,5"
-Lucky63[10] = "1,2,3,6"
-Lucky63[11] = "1,2,4,5"
-Lucky63[12] = "1,2,4,6"
-Lucky63[13] = "1,2,5,6"
-Lucky63[14] = "1,3,4,5"
-Lucky63[15] = "1,3,4,6"
-Lucky63[16] = "1,4,5,6"
-Lucky63[17] = "2,3,4,5"
-Lucky63[18] = "2,3,5,6"
-Lucky63[19] = "2,4,5,6"
-Lucky63[20] = "3,4,5,6"
-Lucky63[21] = "2,3,4,6"
-Lucky63[22] = "1,3,4,6"
-Lucky63[23] = "1,2,3"
-Lucky63[24] = "1,2,4"
-Lucky63[25] = "1,2,5"
-Lucky63[26] = "1,2,6"
-Lucky63[27] = "1,3,4"
-Lucky63[28] = "1,3,5"
-Lucky63[29] = "1,3,6"
-Lucky63[30] = "1,4,5"
-Lucky63[31] = "1,4,6"
-Lucky63[32] = "1,5,6"
-Lucky63[33] = "2,3,4"
-Lucky63[34] = "2,3,5"
-Lucky63[35] = "2,3,6"
-Lucky63[36] = "2,4,5"
-Lucky63[37] = "2,4,6"
-Lucky63[38] = "2,5,6"
-Lucky63[39] = "3,4,5"
-Lucky63[40] = "3,4,6"
-Lucky63[41] = "3,5,6"
-Lucky63[42] = "4,5,6"
-Lucky63[43] = "1,2"
-Lucky63[44] = "1,3"
-Lucky63[45] = "1,4"
-Lucky63[46] = "1,5"
-Lucky63[47] = "1,6"
-Lucky63[48] = "2,3"
-Lucky63[49] = "2,4"
-Lucky63[50] = "2,5"
-Lucky63[51] = "2,6"
-Lucky63[52] = "3,4"
-Lucky63[53] = "3,5"
-Lucky63[54] = "3,6"
-Lucky63[55] = "4,5"
-Lucky63[56] = "4,6"
-Lucky63[57] = "5,6"
-Lucky63[58] = "1"
-Lucky63[59] = "2"
-Lucky63[60] = "3"
-Lucky63[61] = "4"
-Lucky63[62] = "5"
-Lucky63[63] = "6"
+var Lucky31 = new Array[]
+Lucky31[1] = "1,2,3,4,5";
+Lucky31[2] = "1,2,3,4";
+Lucky31[3] = "1,2,3,5";
+Lucky31[4] = "1,2,4,5";
+Lucky31[5] = "1,3,4,5";
+Lucky31[6] = "2,3,4,5";
+Lucky31[7] = "2,4,5";
+Lucky31[8] = "1,2,3";
+Lucky31[9] = "1,2,4";
+Lucky31[10] = "1,2,5";
+Lucky31[11] = "1,3,4";
+Lucky31[12] = "1,3,5";
+Lucky31[13] = "1,4,5";
+Lucky31[14] = "2,3,4";
+Lucky31[15] = "2,3,5";
+Lucky31[16] = "3,4,5";
+Lucky31[17] = "1,2";
+Lucky31[18] = "1,3";
+Lucky31[19] = "1,4";
+Lucky31[20] = "1,5";
+Lucky31[21] = "2,3";
+Lucky31[22] = "2,4";
+Lucky31[23] = "2,5";
+Lucky31[24] = "3,4";
+Lucky31[25] = "3,5";
+Lucky31[26] = "4,5";
+Lucky31[27] = "1";
+Lucky31[28] = "2";
+Lucky31[29] = "3";
+Lucky31[30] = "4";
+Lucky31[31] = "5";
 
+var Lucky63 = new Array[]
+Lucky63[1] = "1,2,3,4,5,6";
+Lucky63[2] = "1,2,3,4,5";
+Lucky63[3] = "1,2,3,4,6";
+Lucky63[4] = "1,2,3,5,6";
+Lucky63[5] = "1,2,4,5,6";
+Lucky63[6] = "1,3,4,5,6";
+Lucky63[7] = "2,3,4,5,6";
+Lucky63[8] = "1,2,3,4";
+Lucky63[9] = "1,2,3,5";
+Lucky63[10] = "1,2,3,6";
+Lucky63[11] = "1,2,4,5";
+Lucky63[12] = "1,2,4,6";
+Lucky63[13] = "1,2,5,6";
+Lucky63[14] = "1,3,4,5";
+Lucky63[15] = "1,3,4,6";
+Lucky63[16] = "1,4,5,6";
+Lucky63[17] = "2,3,4,5";
+Lucky63[18] = "2,3,5,6";
+Lucky63[19] = "2,4,5,6";
+Lucky63[20] = "3,4,5,6";
+Lucky63[21] = "2,3,4,6";
+Lucky63[22] = "1,3,4,6";
+Lucky63[23] = "1,2,3";
+Lucky63[24] = "1,2,4";
+Lucky63[25] = "1,2,5";
+Lucky63[26] = "1,2,6";
+Lucky63[27] = "1,3,4";
+Lucky63[28] = "1,3,5";
+Lucky63[29] = "1,3,6";
+Lucky63[30] = "1,4,5";
+Lucky63[31] = "1,4,6";
+Lucky63[32] = "1,5,6";
+Lucky63[33] = "2,3,4";
+Lucky63[34] = "2,3,5";
+Lucky63[35] = "2,3,6";
+Lucky63[36] = "2,4,5";
+Lucky63[37] = "2,4,6";
+Lucky63[38] = "2,5,6";
+Lucky63[39] = "3,4,5";
+Lucky63[40] = "3,4,6";
+Lucky63[41] = "3,5,6";
+Lucky63[42] = "4,5,6";
+Lucky63[43] = "1,2";
+Lucky63[44] = "1,3";
+Lucky63[45] = "1,4";
+Lucky63[46] = "1,5";
+Lucky63[47] = "1,6";
+Lucky63[48] = "2,3";
+Lucky63[49] = "2,4";
+Lucky63[50] = "2,5";
+Lucky63[51] = "2,6";
+Lucky63[52] = "3,4";
+Lucky63[53] = "3,5";
+Lucky63[54] = "3,6";
+Lucky63[55] = "4,5";
+Lucky63[56] = "4,6";
+Lucky63[57] = "5,6";
+Lucky63[58] = "1";
+Lucky63[59] = "2";
+Lucky63[60] = "3";
+Lucky63[61] = "4";
+Lucky63[62] = "5";
+Lucky63[63] = "6";
+
+var stake = eval(document.bettingcalculator.stake.value);
+
+var a = eval(document.bettingcalculator.bet1fraction1.value);
+var b = eval(document.bettingcalculator.bet1fraction2.value);
+var c = eval(document.bettingcalculator.bet2fraction1.value);
+var d = eval(document.bettingcalculator.bet2fraction2.value);
+var e = eval(document.bettingcalculator.bet3fraction1.value);
+var f = eval(document.bettingcalculator.bet3fraction2.value);
+var g = eval(document.bettingcalculator.bet4fraction1.value);
+var h = eval(document.bettingcalculator.bet4fraction2.value);
+var i = eval(document.bettingcalculator.bet5fraction1.value);
+var j = eval(document.bettingcalculator.bet5fraction2.value);
+var k = eval(document.bettingcalculator.bet6fraction1.value);
+var l = eval(document.bettingcalculator.bet6fraction2.value);
+var bet1 = a / b;
+var bet2 = c / d;
+var bet3 = e / f;
+var bet4 = g / h;
+var bet5 = i / j;
+var bet6 = k / l;
+
+var profit = winnings - stake;
+var trixiestake = stake * 4;
+var trixieprofit = winnings - trixiestake;
+var patentstake = stake * 7;
+var patentprofit = winnings - patentstake;
+var yankeestake = stake * 11;
+var yankeeprofit = winnings - yankeestake;
+var lucky15stake = stake * 15;
+var lucky15profit = winnings - lucky15stake;
+var canadianstake = stake * 26;
+var canadianprofit = winnings - canadianstake;
+var lucky31stake = stake * 31;
+var lucky31profit = winnings - lucky31stake;
+var lucky63stake = stake * 63;
+var lucky63profit = winnings - lucky63stake;
+
+// Calculations for each bet type
+
+var i = getSelectedButton(document.bettingcalculator.bet_type)
+var j = getBetFormatButton(document.bettingcalculator.bet_format)
+
+if (document.bettingcalculator.bet_type[i].value == "Single") {
+    var bets_out = new Array("5","4","3","2","1");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "1")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Double") {
+    var bets_out = new Array("5","4","3","2");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "1")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Trixie") {
+    var bets_out = new Array("5","4","3");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "4")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Lucky15") {
+    var bets_out = new Array("5","4");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "15")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Patent") {
+    var bets_out = new Array("5","4","3");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "7")
+        winnings = updated_winnings;
+    }
+else if (document.bettingcalculator.bet_type[i].value == "Yankee") {
+    var bets_out = new Array("5","4");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "11")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Canadian") {
+    var bets_out = new Array("5");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "26")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Lucky31") {
+    var bets_out = new Array("5");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "31")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Heinz") {
+    var bets_out = new Array();
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "57")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Lucky63") {
+    var bets_out = new Array();
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "63")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Treble") {
+    var bets_out = new Array("5","4","3");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+
+    } else {
+        normalBet(document.bettingcalculator.bet_type[i].value, document.bettingcalculator.bet_format[j].value, "1")
+        winnings = updated_winnings;
+    }
+
+else if (document.bettingcalculator.bet_type[i].value == "Accumulator") {
+    var bets_out = new Array("5","4");
+    delete_bets(document.bettingcalculator.bet_type[i].value, bets_out, document.bettingcalculator.bet_format[j].value);
+    if (b4 || d4 > 0) {
+            normalBet("Accumulator4", document.bettingcalculator.bet_format[j].value, "1")
+            winnings = updated_winnings;
+        }
+
+    }
+    {
+        if (b4 && b5 || d4 && d5 > 0) {
+                normalBet("Accumulator5", document.bettingcalculator.bet_format[j].value, "1")
+                winnings = updated_winnings;
+            }
+
+        }
+        if (b4 && b5 && b6 || d4 && d5 && d6 > 0) {
+                normalBet("Accumulator6", document.bettingcalculator.bet_format[j].value, "1")
+                winnings = updated_winnings;
+            }
+        
 
 
 
@@ -235,5 +400,4 @@ Lucky63[63] = "6"
 // console.log(trixieReturns + " - Trixie returns");       //800
 // console.log(yankeeReturns + " - Yankee returns");       //5100
 // console.log(canadianReturns + " - Canadian returns");   //31000
-
 
